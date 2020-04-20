3665fcb9edc51a0e567edff650e0abf13bc9feb3
mmolano <miike.molano@gmail.com>
2020-04-20 14:35:10 +0200

    Structure need to add data

---
+++ components/UserDropdown.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <a
+      class="text-gray-600 block"
+      href="#pablo"
+      v-on:click="toggleDropdown($event)"
+      ref="btnDropdownRef"
+    >
+      <div class="items-center flex">
+        <span
+          class="w-12 h-12 text-sm text-white bg-gray-300 inline-flex items-center justify-center rounded-full"
+        >
+          <img
+            alt="..."
+            class="w-full rounded-full align-middle border-none shadow-lg"
+            src="../assets/img/team-1-800x800.jpg"
+          />
+        </span>
+      </div>
+    </a>
+    <div
+      ref="popoverDropdownRef"
+      class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1"
+      v-bind:class="{
+        hidden: !dropdownPopoverShow,
+        block: dropdownPopoverShow
+      }"
+      style="min-width: 12rem"
+    >
+      <a
+        href="#pablo"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Profile
+      </a>
+      <a
+        href="#pablo"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Data
+      </a>
+      <a
+        href="#pablo"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+       Contact
+      </a>
+      <div class="h-0 my-2 border border-solid border-gray-200" />
+      <a
+        href="#pablo"
+        class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent text-gray-800"
+      >
+        Logout
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+import { createPopper } from '@popperjs/core';
+
+export default {
+  data() {
+    return {
+      dropdownPopoverShow: false
+    };
+  },
+  methods: {
+    toggleDropdown: function(event) {
+      event.preventDefault();
+      if (this.dropdownPopoverShow) {
+        this.dropdownPopoverShow = false;
+      } else {
+        this.dropdownPopoverShow = true;
+        createPopper(this.$refs.btnDropdownRef, this.$refs.popoverDropdownRef, {
+          placement: "bottom-end"
+        });
+      }
+    }
+  }
+};
+</script>
